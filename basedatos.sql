195c3df84fcd2e9a3d0ace246f3a9571916aa090
David_Bueno <david03bueno@gmail.com>
2024-12-05 00:57:13 -0400

    Agregando base de datos

--- basedatos.sql
+++ basedatos.sql
@@ -1,2 +1,13 @@
-use proyecto_final_electiva1,
-select * from usuarios
+create database proyecto_Final_Electiva1
+use proyecto_Final_Electiva1
+CREATE TABLE USUARIOS
+(
+Id_Usuario int PRIMARY KEY AUTO_INCREMENT,
+Usuario Nvarchar(50),
+Cargo Nvarchar(50),
+Sueldo decimal
+)
+
+INSERT INTO USUARIOS(Usuario,Cargo,Sueldo)
+values
+('Juan','Supervisor',100)
